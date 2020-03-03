877bc651d4104abe63bcc0fd3b4fb7cd2b501f5b
Mr0Strange1pwn <Pawan.Tiwari@mechlintech.com>
2020-03-03 06:50:30 +0530

    Updates

---
+++ precache-manifest.93e3dfc6bdfa501c771623c39c513f26.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8663d35a52fd9aa1d889392244b466",
+    "url": "/mt-portal-demo/index.html"
+  },
+  {
+    "revision": "d5bfc4941ce1507b0104",
+    "url": "/mt-portal-demo/static/css/main.bd9069b2.chunk.css"
+  },
+  {
+    "revision": "4ab8f34d8f4628f413af",
+    "url": "/mt-portal-demo/static/js/2.dc0b76f3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mt-portal-demo/static/js/2.dc0b76f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5bfc4941ce1507b0104",
+    "url": "/mt-portal-demo/static/js/main.a6277127.chunk.js"
+  },
+  {
+    "revision": "87bef9fe7f7fecec5557",
+    "url": "/mt-portal-demo/static/js/runtime-main.21d00e5f.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/mt-portal-demo/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/mt-portal-demo/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/mt-portal-demo/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/mt-portal-demo/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "cc17bc2bf08c88521288aa8f576f5473",
+    "url": "/mt-portal-demo/static/media/img-02.cc17bc2b.png"
+  }
+]);
